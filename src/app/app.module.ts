195eda0d992585d719047b9c3e61308dbd72f89e
Your Name <rahultiger1998@gmail.com>
2022-05-20 16:36:51 +0530

    First commit card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyCardComponent } from './components/pages/my-card/my-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCardComponent
   ],
   imports: [
     BrowserModule
